bc8bc283f16d86773ea4c6cee43e38025446ac67
amnaikram01 <117072846+amnaikram01@users.noreply.github.com>
2024-11-26 15:17:58 +0500

    Add files via upload

---
+++ repo.py
@@ -0,0 +1,6 @@
+#program to take input from user and return if the number is even or odd
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num,"is even")
+else:
+    print(num,"is odd")
